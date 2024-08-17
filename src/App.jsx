02e4d2ecac5b50f6ac38a64308de9831667bbb5a
ghost <ghost>
2024-08-18 00:32:39 +0200

    Hello

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+
+const App = () => {
+  return (
+    <>
+    <Navbar />
+    <Hero title='Test Title' subtitle='Test subtilte' />
+    <HomeCards />
+    <JobListings />
+
+ 
+
+
+    
+    
+    
+    </>
+  )
+}
+
+export default App;

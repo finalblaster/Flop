3900fcc14c19be80a1a37097d3efc6fbcc77d55a
finalblaster <ghost>
2024-08-19 20:26:15 +0200

    job page

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,15 @@ import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout'
 import JobPages from './pages/JobPages';
 import NotFoundPage from './pages/NotFoundPage';
+import JobPage, {jobLoader} from './pages/JobPage'
 
 const router =  createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element={<MainLayout />}>
     <Route index element={<HomePage />} />
-    <Route path='/jobs' element={<JobPages />} />
+    <Route path='/jobs' element={<JobPages />}/>
+    {/* <Route path='/jobs/:id' element={<JobPage />} /> */}
+    <Route path='/jobs/:id' element={<JobPage />} loader = {jobLoader}/>
     <Route path='*' element={<NotFoundPage />}/>
   </Route>
   )
